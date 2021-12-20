be9f0ffb3ae958930731adcbe7e8ca0b3aa03cb3
Jun Wei Wang <jwwang.2003@gmail.com>
2021-12-20 04:46:45 -0800

    ahhh gayyyyy fixed Error: Using target="_blank" without rel="noreferrer" (which implies rel="noopener") is a security risk in older browsers: see https://mathiasbynens.github.io/rel-noopener/#recommendations  react/jsx-no-target-blank

--- components/HeaderCard.js
+++ components/HeaderCard.js
@@ -20,7 +20,7 @@ const RedirectWarning = ({ closeToast, url }) => (
     You are about to be redirected to <p className="text-blue-500">{url}</p>
     <div>
       <button onClick={closeToast} type="button" className="py-2 px-4 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Nah</button>
-      <a href={url} target="_blank">
+      <a href={url} target="_blank" rel="noreferrer">
         <button type="button" className="py-2 px-4 mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Procced</button>
       </a>
     </div>
